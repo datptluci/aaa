46a7ff35193d42a4b03cf33bb86a52629b17503f
datptluci <datpt@luci.vn>
2022-06-03 09:40:38 +0700

    null

--- nl-routing/src/main/java/com/luci/nl/routing/employee/paging/EmployeePagination.java
+++
@@ -1,9 +0,0 @@
-package com.luci.nl.routing.employee.paging;
-
-public class EmployeePagination {
-    private int pageNo;
-    private int pageSize;
-    private long totalElements;
-    private int totalPages;
-    private boolean last;
-}
